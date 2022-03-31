8cb6d4be43fcf9a0dc95c7e6485130b987dc2e0a
Anushka123 <aokunev@uci.edu>
2022-04-01 10:49:16 +1100

    made changes to arduino sketch. Added if statements and made a little dance

--- moving_wink_one/moving_wink_one.ino
+++ moving_wink_one/moving_wink_one.ino
@@ -23,7 +23,8 @@ Visit http://www.arduino.cc to learn about the Arduino.
                            // machine language Wink can understand.
 
 
-
+  byte button;
+  
 
 
 // Below is the "setup" function. It runs one time as soon as Wink turns on. You can add stuff
@@ -33,18 +34,55 @@ void setup(){
   hardwareBegin();        //initialize Wink's brain to work with his circuitry
   playStartChirp();       //Play startup chirp and blink eyes
   
-  RxIRRestart(4);         //wait for 4 byte IR remote command
+  RxIRRestart(4); 
+  
+
 
   //IR_Play 20
 }
 
-byte button;
+  // read which button was pressed
           
 void loop(){
 
   if(IsIRDone()){             // if an IR packet has been received...
-    button = GetIRButton();   // read which button was pressed
-    Serial.println(button);
+      button = GetIRButton();//wait for 4 byte IR remote command
+      Serial.println(button);
+
+      if(button == 20){
+        motors(150, 150); //move forward for 1 seconds
+        delay(800);
+        motors(0,0);
+        
+        motors(-255,255); //spin for 2 seconds
+        delay(2000);
+        motors(0,0);
+
+        motors(-150,-150); //move back for 2 seconds
+        delay(800);
+        motors(0,0);
+
+        motors(-150, 150); //move left 1 second
+        delay(1000);
+        motors(0,0);
+
+        motors(150, -150); //move right 1 second
+        delay(1000);
+        motors(0,0);
+
+        motors(-255,255); //spin for 2 seconds
+        delay(2000);
+        motors(0,0);
+        RxIRRestart(4);
+        
+      }
+
+  else{
+    RxIRRestart(4);
+  }
+
+     
+    
 
  
 
